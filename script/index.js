af52da93f5b3e4df3e73fe948ea373ac05602d88
Muhammad Kassar <36678834+mk360@users.noreply.github.com>
2024-08-21 01:25:30 +0200

    revamp ui, finish unit search

--- script/index.js
+++ script/index.js
@@ -6,8 +6,10 @@
     const BARRACKS = document.getElementById("barracks");
     const TABS = document.querySelectorAll('input[name="tab"]');
     // const HERO_SEARCH = document.getElementById("hero-search");
-    // const SEARCH_RESULTS = document.getElementById("search-results");
+    const SEARCH_RESULTS = document.getElementById("search-content");
     const SKILLS_DIALOG = document.getElementById("inheritable-skills");
+    const HERO_SEARCH = document.getElementById("search-query");
+    let searchQuery = "";
 
     // SKILLS_DIALOG.oncancel = function(e) {
     //     e.preventDefault();
@@ -27,14 +29,6 @@
         }
     }
 
-    function getButton(element) {
-        let node = element;
-        while (node.nodeName !== "BUTTON" && node.parentNode) {
-            node = node.parentNode;
-        }
-        return node;
-    }
-
     function checkInheritableSkills() {
         SKILLS_DIALOG.showModal();
         const { unitId } = this.dataset;
@@ -43,40 +37,17 @@
         for (let id of existingIds) {
             urlSearchParams.append("intIDs", id);
         }
-        fetch(`http://localhost:3333/skills?slot=A&searchedId=${unitId}&mode=roster&${urlSearchParams.toString()}`).then((res) => res.json()).then(console.log)
+        fetch(`http://localhost:3333/skills?slot=A&searchedId=${unitId}&mode=roster&${urlSearchParams.toString()}`).then((res) => {
+            return res.json();
+        }).then(console.log)
     }
-
-    // SEARCH_RESULTS.onclick = (e) => {
-    //     const btn = getButton(e.target);
-    //     const { unitId } = btn.dataset;
-    //     createBarracksItem(unitId);
-    //     localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)).concat(+unitId)));
-    //     while (SEARCH_RESULTS.childNodes.length) SEARCH_RESULTS.removeChild(SEARCH_RESULTS.firstChild);
-    //     SEARCH_RESULTS.classList.add("hide");
-    // }
     
-    // HERO_SEARCH.onkeyup = (e) => {
-    //     const value = e.target.value;
-    //     if (value.length < 3) {
-    //         SEARCH_RESULTS.classList.add("hide");
-    //         return;
-    //     }
-    //     const existingIds = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
-    //     const urlSearchParams = new URLSearchParams();
-    //     urlSearchParams.set("query", encodeURIComponent(value));
-    //     for (let id of existingIds) {
-    //         urlSearchParams.append("ids", id);
-    //     }
-    //     fetch(`http://localhost:3333/heroes?${urlSearchParams.toString()}`, {
-    //         signal: controller.signal
-    //     }).then((response) => response.json()).then((entries) => {
-    //         while (SEARCH_RESULTS.childNodes.length) SEARCH_RESULTS.removeChild(SEARCH_RESULTS.firstChild);
-    //         SEARCH_RESULTS.classList.remove("hide");
-    //         for (let entry of entries) {
-    //             createSearchItem(entry);
-    //         }
-    //     }).catch(() => {});
-    // }
+    HERO_SEARCH.onkeyup = (e) => {
+        searchQuery = e.target.value;
+        if (searchQuery.length > 2 || !searchQuery.length) {
+            loadSearchSuggestions(0);
+        }
+    }
 
     for (let color of ["Red", "Blue", "Green", "Colorless"]) {
         for (let weapon of ["Bow", "Tome", "Breath", "Beast", "Dagger"]) {
@@ -90,31 +61,75 @@
         loadBarracks();
     }
 
+    loadSearchSuggestions(0);
+
     function loadBarracks() {
         const items = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
         if (items.length) {
-            for (let element of items) {
-                if (element) createBarracksItem(element);
+            for (let savedId of items) {
+                if (savedId) {
+                    const { heroButton, iconsContainer } = createHeroItem(savedId, true);
+                    const deleteButton = createDeleteIcon();
+
+                    deleteButton.onclick = handleDeleteHeroEvent(savedId, heroButton);
+
+                    iconsContainer.appendChild(deleteButton);
+                    BARRACKS.appendChild(heroButton);
+                }
             }
         }
     }
 
-    function createSearchItem(searchEntry) {
-        const button = document.createElement("button");
-        button.classList.add("search-entry");
-        const img = document.createElement("img");
-        img.src = `http://localhost:3333/img?id=${searchEntry.id}&imgType=portrait`;
-        img.loading = "lazy";
-        const span = document.createElement("span");
-        span.innerHTML = searchEntry.name;
-        button.appendChild(img);
-        button.appendChild(span);
-        button.dataset.unitId = searchEntry.id;
-        SEARCH_RESULTS.appendChild(button);
+    function handleDeleteHeroEvent(unitId, boundItem) {
+        return function deleteFromBarracks(e) {
+            e.stopPropagation();
+            const { heroButton } = createHeroItem(unitId);
+            SEARCH_RESULTS.prepend(heroButton);
+            BARRACKS.removeChild(boundItem);
+            saveBarracks();
+            heroButton.onclick = addToBarracks;
+        }
     }
 
-    function createBarracksItem(heroId) {
-        // const div = document.createElement("div");
+    function saveBarracks() {
+        const heroes = BARRACKS.getElementsByClassName("hero-container");
+        const mappedIds = Array.prototype.map.call(heroes, (hero) => hero.dataset.unitId);
+        localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(mappedIds));
+    }
+
+    function addToBarracks() {
+        const newButtons = createHeroItem(this.dataset.unitId, true);
+        const deleteIcon = createDeleteIcon();
+        deleteIcon.onclick = handleDeleteHeroEvent(this.dataset.unitId, newButtons.heroButton);
+        newButtons.iconsContainer.appendChild(deleteIcon);
+        BARRACKS.appendChild(newButtons.heroButton);
+        saveBarracks();
+        SEARCH_RESULTS.removeChild(this);
+    }
+
+    function loadSearchSuggestions(page) {
+        const items = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+        const params = new URLSearchParams();
+        for (let item of items) {
+            params.append("ids", item);
+        }
+        if (searchQuery.trim().length) {
+            params.append("query", searchQuery);
+        }
+        params.append("page", page);
+
+        return fetch(`http://localhost:3333/heroes?${params.toString()}`).then((response) => response.json()).then((elements) => {
+            while (SEARCH_RESULTS.lastChild.nodeName !== "DIV") SEARCH_RESULTS.removeChild(SEARCH_RESULTS.lastChild);
+
+            for (let elem of elements) {
+                const { heroButton } = createHeroItem(elem);
+                SEARCH_RESULTS.appendChild(heroButton);
+                heroButton.onclick = addToBarracks;
+            }
+        });
+    }
+
+    function createHeroItem(heroId, addIcons) {
         const heroButton = document.createElement("button");
         heroButton.classList.add("hero-container");
         const frame = document.createElement("img");
@@ -126,9 +141,28 @@
         img.loading = "lazy";
         img.classList.add("portrait");
         heroButton.appendChild(img);
-        heroButton.appendChild(frame);
-        heroButton.onclick = checkInheritableSkills;
         heroButton.dataset.unitId = heroId;
-        BARRACKS.appendChild(heroButton);
+        let iconsContainer = null;
+        iconsContainer = document.createElement("div");
+        iconsContainer.classList.add("icons-container");
+        iconsContainer.appendChild(frame);
+
+        if (addIcons) {
+            heroButton.appendChild(iconsContainer);
+        } else {
+            heroButton.appendChild(frame);
+        }
+
+        return { heroButton, iconsContainer };
+    }
+
+    function createDeleteIcon() {
+        const deleteButton = document.createElement("div");
+        deleteButton.classList.add("delete-button");
+        const img = document.createElement("img");
+        img.src = "./static/trash.png";
+        deleteButton.appendChild(img);
+
+        return deleteButton;
     }
 })();
